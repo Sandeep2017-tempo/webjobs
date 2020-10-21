dcb06a80dea35fce62591bcbdf8d1abf442794b8
Sandeep2017-tempo <70652304+Sandeep2017-tempo@users.noreply.github.com>
2020-10-21 20:05:00 +0530

    new changes

---
+++ webjobsdksample/bin/Debug/netcoreapp3.1/publish/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Extensions.Storage;
+
+
+
+namespace webjobsdksample
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var builder = new HostBuilder();
+            builder.UseEnvironment(EnvironmentName.Development);
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+
+                // If the key exists in settings, use it to enable Application Insights.
+                string instrumentationKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                if (!string.IsNullOrEmpty(instrumentationKey))
+                {
+                    b.AddApplicationInsightsWebJobs(o => o.InstrumentationKey = instrumentationKey);
+                }
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
